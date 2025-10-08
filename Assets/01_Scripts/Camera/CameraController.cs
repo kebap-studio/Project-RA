ade825c038448496446b72781c54a03d253b0dee
tolelom <98kimsungmin@naver.com>
2025-10-08 11:17:22 +0900

    feat: 샘플 씬에 바닥, 카메라, 캐릭터 이동 구현 추가
    - 바닥 평면 추가 및 기본 환경 구성
    - PlayerInput과 CharacterController를 이용한 캐릭터 이동 기능 구현
    - 입력 디버그 출력 기능 추가

---
+++ Assets/01_Scripts/Camera/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+    public class CameraController : MonoBehaviour
+    {
+        [Header("Camera Settings")] 
+        [SerializeField] private float fieldOfView = 60f;
+        
+        private UnityEngine.Camera _camera;
+
+        private void Start()
+        {
+            _camera = GetComponent<UnityEngine.Camera>();
+            _camera.fieldOfView = fieldOfView;
+            
+            transform.position = new Vector3(0f, 15f, -10f);
+            transform.rotation = Quaternion.Euler(45f, 0f, 0f);
+        }
+    }
---
+++ Assets/01_Scripts/Core/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+    public class GameManager : MonoBehaviour
+    {
+        private static GameManager Instance { get; set; }
+        
+        [Header("Game State")]
+        [SerializeField] private bool isPaused = false;
+
+        [Header("References")] 
+        [SerializeField] private PlayerController player;
+        [SerializeField] private CameraController cameraController;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+                InitializeGame();
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private void InitializeGame()
+        {
+            Debug.Log("Game Initialized!");
+        }
+
+        
+        public void PauseGame()
+        {
+            isPaused = true;
+            Time.timeScale = 0f;
+        }
+
+        public void ResumeGame()
+        {
+            isPaused = false;
+            Time.timeScale = 1f;
+        }
+        
+        public bool IsPaused() => isPaused;
+    }
