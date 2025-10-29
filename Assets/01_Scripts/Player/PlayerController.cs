18d7410d054edb85d289539486667566655615fe
qornwh <qornwh@gmail.com>
2025-10-29 16:07:22 +0900

    feat : 캐릭터 임시 애니매이션 팩 에셋 추가, 윤곽 쉐이더 구현, 클릭 이동 기능 추가  - mixamo : 검, 마법, 기본 애니메이션 추가  - 쉐이레더 : 오브젝트 윤곽, 포스트 프로세스 윤곽선 그리기 추가  - 플레이어 : 캐릭터 클릭 이동, 가속도 임시 추가

--- Assets/01_Scripts/Player/PlayerController.cs
+++ Assets/01_Scripts/Player/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Unity.Mathematics.Geometry;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 
@@ -24,6 +25,12 @@ public class PlayerController : MonoBehaviour
     // Movement
     private Vector3 _moveDirection;
     private Vector3 _velocity;
+    private float _acceleration = 1.5f;
+    private float _deceleration = 5f;
+
+    // Attack Move
+    private Vector3 _lastPosition;
+    private bool _isLastPosition = false;
 
     private bool _isMoving = false;
     private bool _wasMoving = false;
@@ -62,13 +69,38 @@ public class PlayerController : MonoBehaviour
 
         float currentSpeed = moveSpeed;
 
-        _velocity = _moveDirection * currentSpeed;
+        if (_moveDirection.magnitude > 0.1f)
+        {
+            // 가속
+            // _velocity = Vector3.MoveTowards(_velocity, _moveDirection * currentSpeed, Time.deltaTime * _acceleration);
+            // _velocity = _moveDirection * currentSpeed;
+            _velocity = Vector3.Lerp(_velocity, _moveDirection * currentSpeed, Time.deltaTime * _acceleration);
+        }
+        else
+        {
+            // 감속
+            // 그냥 멈추기로
+            // _velocity = Vector3.MoveTowards(_velocity, Vector3.zero, Time.deltaTime * _deceleration);
+            // _velocity = Vector3.zero;
+            float smoothTime = 0.01f;
+            _velocity = Vector3.SmoothDamp(_velocity, Vector3.zero, ref _velocity, smoothTime);
+        }
         _characterController.Move(_velocity * Time.deltaTime);
 
         if (_animator != null)
         {
             // TODO: Animator 설정
         }
+
+        if (_isLastPosition)
+        {
+            float distanceToLastPos = Vector3.Distance(transform.position, _lastPosition);
+            if (distanceToLastPos < 0.1f)
+            {
+                _isLastPosition = false;
+                _moveInput = Vector2.zero;
+            }
+        }       
     }
 
     private void HandleRotation()
@@ -105,12 +137,39 @@ public class PlayerController : MonoBehaviour
     }
 
     // Input System Events
-    void OnMove(InputValue value) 
+    void OnMove(InputValue value)
     {
         _moveInput = value.Get<Vector2>();
+        _isLastPosition = false;
 
         if (showDebugInfo)
             Debug.Log($"Move Input: {_moveInput}");
+    }
+
+    void OnAttack(InputValue value)
+    {
+        Vector2 mouseScreenPos = Mouse.current.position.ReadValue();
+        Ray ray = Camera.main.ScreenPointToRay(mouseScreenPos);
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, 100f))
+        {
+            Vector3 worldMousePos = hitInfo.point;
+            worldMousePos.y = transform.position.y; // 플레이어 높이에 맞춤
+
+            Vector3 direction = (worldMousePos - transform.position).normalized;
+            Debug.DrawRay(transform.position, direction * 5f, Color.green, 2f);
+            Vector2 dirction2D = new Vector2(direction.x, direction.z);
+
+            if (Vector3.Distance(worldMousePos, transform.position) < 0.1f)
+            {
+                // TODO: hitInfo 타겟 확인, 회전, 공격
+            }
+            else
+            {
+                _moveInput = dirction2D;
+                _lastPosition = worldMousePos;
+                _isLastPosition = true;
+            }            
+        }
     }
 
     // 외부에서 호출 가능한 메서드들
